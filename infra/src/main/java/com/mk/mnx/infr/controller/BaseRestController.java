18b67cb9922d3f7bbb15a194ac78e57d7c78e5d3
mgldrz@gmail.com <mgldrz@gmail.com>
2018-02-12 22:19:56 -0600

    se agrega framework de validaciones

--- infra/src/main/java/com/mk/mnx/infr/controller/BaseRestController.java
+++ infra/src/main/java/com/mk/mnx/infr/controller/BaseRestController.java
@@ -1,6 +1,8 @@
 package com.mk.mnx.infr.controller;
 
 import java.io.IOException;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.mk.mnx.infr.constants.CommonConstants;
 import com.mk.mnx.infr.exception.HttpCodeException;
+import com.mk.mnx.vld.exception.MonoxValidationConstraintException;
 
 public abstract class BaseRestController {
 	
@@ -45,6 +48,18 @@ public abstract class BaseRestController {
     public void handleHttpError(HttpServletRequest req, HttpServletResponse resp , HttpCodeException ex) throws IOException {
     	loggerException.error("Request: [{}] message: [{}]" , req.getRequestURL() , ex.getMessage());
     	resp.sendError(ex.getHttpCode(),ex.getMessage());
+
+    }
+    
+    @ExceptionHandler(UndeclaredThrowableException.class)
+    public void handleUndeclaredThrowableException(HttpServletRequest req, HttpServletResponse resp , UndeclaredThrowableException ex) throws IOException {
+    	loggerException.error("Request: [{}] message: [{}]" , req.getRequestURL() , ex.getMessage());
+    	resp.sendError(HttpServletResponse.SC_BAD_REQUEST,ex.getMessage());
+    	if(ex.getUndeclaredThrowable() instanceof MonoxValidationConstraintException) {
+    		resp.sendError(HttpServletResponse.SC_BAD_REQUEST,ex.getMessage());
+    		MonoxValidationConstraintException mvce = (MonoxValidationConstraintException) ex.getUndeclaredThrowable();
+    		mvce.getErrors().stream().map( f -> f.getMessage() ).collect(Collectors.toList());
+    	}
 
     }
     
